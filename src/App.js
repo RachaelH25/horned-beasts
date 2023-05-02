bd4e367fa0c410564339a05b2ba7c05574c28014
Rachael Hindley <rachael.hindley52@gmail.com>
2023-05-02 15:09:35 +0100

    unfinished attempt at lab01

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <header></header>
+            <main></main>
+            <footer></footer>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <div className="footer">
+            <p>By Rachael</p>
+        </div>
+    );
+}
+
+export default Footer;
